744cb88633548e0aeee8972532536892dc5e5c6d
Sean Kelley <seansfkelley@users.noreply.github.com>
2018-09-12 19:34:08 -0700

    Use conditional types to make types significantly safer and more robust. (#108)
    * Update typings using conditional types to get significantly safer typings.
    
    See the PR description for more details.
    
    * Add a bunch more tests.
    
    * Fix node-version-checking hack to work properly.
    
    - Coerces the major version into a number to ensure proper comparisons with the literal 4.
    - Can actually fail now; previously, it would always return 0 because the ternary was
      parenthesized wrong. The condition would get executed, causing the process to immediately
      exit, and the true/false branches were never hit.
    - Invert the conditional check so that it can be chained in the shell command properly.
    - Quiet the command so that it doesn't log concerning and misleading error messages in the
      build output.
    
    Additionally, I turned off the filename casing check because it wasn't helping and was
    failing the build.
    
    * Reorganize types a bit to localize related things and remove indirection.
    
    * Add tests for union and intersection types.

--- index.d.ts
+++ index.d.ts
@@ -1,55 +1,69 @@
 // Project: Immutability helper
-// TypeScript Version: 2.2
+// TypeScript Version: 2.9
 
-export default update
+export default update;
 
-declare function update<T>(
-  data: ReadonlyArray<T>,
-  query: ArrayOperators<T>,
-): ReadonlyArray<T>
-
-declare function update<T>(
-  data: ReadonlySet<T>,
-  query: SetOperators<T>,
-): ReadonlySet<T>
-
-declare function update<K, V>(
-  data: ReadonlyMap<K, V>,
-  query: MapOperators<K, V>,
-): ReadonlyMap<K, V>
-
-declare function update<T>(data: T, query: Query<T>): T
-
-type Tree<T> = {[K in keyof T]?: Query<T[K]>}
-export type Query<T> =
-  | Tree<T>
-  | ObjectOperators<T>
-  | ArrayOperators<any>
-  | SetOperators<any>
-
-type ObjectOperators<T> =
-  | {$set: any}
-  | {$toggle: Array<keyof T | number>}
-  | {$unset: Array<keyof T | number>}
-  | {$merge: Partial<T>}
-  | {$apply: (old: T) => T}
-  | ((old: T) => any)
-type ArrayOperators<T> =
-  | {$push: T}
-  | {$unshift: T}
-  | {$splice: Array<[number, number]>}
-  | {[customCommand: string]: any}
-
-type MapOperators<K, V> = {$add: Array<[K, V]>} | {$remove: K[]}
-type SetOperators<T> = {$add: T[]} | {$remove: T[]}
+declare function update<T, C extends CustomCommands<object> = never>(
+  target: T,
+  spec: Spec<T, C>,
+): T;
 
 declare namespace update {
-  function newContext(): typeof update
+  function newContext(): typeof update;
   function extend<T>(
-    command: Command,
-    handler: (param: CommandArg, old: T) => T,
-  ): void
-
-  type Command = string
-  type CommandArg = any
+    command: string,
+    handler: (param: any, old: T) => T,
+  ): void;
 }
+
+// Usage with custom commands is as follows:
+//
+//   interface MyCommands {
+//     $foo: string;
+//   }
+//
+//    update<Foo, CustomCommands<MyCommands>>(..., { $foo: "bar" });
+//
+// It is suggested that if you use custom commands frequently, you wrap and re-export a
+// properly-typed version of `update`:
+//
+//   function myUpdate<T>(object: T, spec: Spec<T, CustomCommands<MyCommands>>) {
+//     return update(object, spec);
+//   }
+//
+// See https://github.com/kolodny/immutability-helper/pull/108 for explanation of why this
+// type exists.
+export type CustomCommands<T> = T & { __noInferenceCustomCommandsBrand: any };
+
+export type Spec<T, C extends CustomCommands<object> = never> =
+  | (
+      T extends (Array<infer U> | ReadonlyArray<infer U>) ? ArraySpec<U, C> :
+      T extends (Map<infer K, infer V> | ReadonlyMap<infer K, infer V>) ? MapSpec<K, V> :
+      T extends (Set<infer U> | ReadonlySet<infer U>) ? SetSpec<U> :
+      T extends object ? ObjectSpec<T, C> :
+      never
+    )
+  | { $set: T }
+  | { $apply: (v: T) => T }
+  | ((v: T) => T)
+  | (C extends CustomCommands<infer O> ? O : never);
+
+type ArraySpec<T, C extends CustomCommands<object>> =
+  | { $push: T }
+  | { $unshift: T }
+  | { $splice: Array<[number] | [number, number] | [number, number, T]> }
+  | { [index: string]: Spec<T, C> }; // Note that this does not type check properly if index: number.
+
+type MapSpec<K, V> =
+  | { $add: Array<[K, V]> }
+  | { $remove: K[] };
+
+type SetSpec<T> =
+  | { $add: T[] }
+  | { $remove: T[] };
+
+type ObjectSpec<T, C extends CustomCommands<object>> =
+  | { $toggle: Array<keyof T> }
+  | { $unset: Array<keyof T> }
+  | { $merge: Partial<T> }
+  | { [K in keyof T]?: Spec<T[K], C> };
